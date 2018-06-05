2317d4f73a3d6057226dc43a7c94257f5b2fd33e
bigknife <bigknife@outlook.com>
2018-06-05 23:37:28 +0800

    add compile stuff to contract service

--- ast/src/main/scala/fssi/ast/domain/ContractService.scala
+++ ast/src/main/scala/fssi/ast/domain/ContractService.scala
@@ -1,6 +1,9 @@
 package fssi.ast.domain
 
-import bigknife.sop._, macros._, implicits._
+import java.nio.file.Path
+
+import bigknife.sop._, implicits._
+import macros._
 import fssi.ast.domain.types._
 
 @sp trait ContractService[F[_]] {
@@ -9,4 +12,13 @@ import fssi.ast.domain.types._
 
   /** run the contract */
   def runContract(invoker: Account, contract: Contract): P[F, Either[Throwable, Moment]]
+
+  /** compile source code to class file */
+  def compileContractSourceCode(source: Path): P[F, Path]
+
+  /** check the contract classes are deterministic */
+  def checkDeterministicOfClass(classFilePath: Path): P[F, Path]
+
+  /** package all contract classes to jar */
+  def jarContract(classFilePath: Path): P[F, BytesValue]
 }
