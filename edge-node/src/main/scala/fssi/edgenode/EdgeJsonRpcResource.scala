23ab1dceb709a4868ae68d207cc0829115648613
bigknife <bigknife@outlook.com>
2018-08-15 17:36:51 +0800

    add json rpc resource

--- edge-node/src/main/scala/fssi/edgenode/EdgeJsonRpcResource.scala
+++ edge-node/src/main/scala/fssi/edgenode/EdgeJsonRpcResource.scala
@@ -17,9 +17,29 @@ trait EdgeJsonRpcResource extends Resource {
   val setting: Setting.EdgeNodeSetting
   val edgeNodeProgram: EdgeNodeProgram[components.Model.Op]
 
-  def contains(method: String): Boolean = true
+  def contains(method: String): Boolean = method match {
+    case "sendTransaction" => true
+    case _ => false
+  }
 
-  def paramsAcceptable(method: String, params: Json): Boolean = true
+  def paramsAcceptable(method: String, params: Json): Boolean = method match {
+    case "sendTransaction" =>
+      if (params.isObject) {
+        val jso = params.asObject.get
+        jso("type") match {
+          case Some(t) if t.isString =>
+            t.asString match {
+              case Some("Transfer") => true
+              case Some("PublishContract") => true
+              case Some("RunContract") => true
+              case _ => false
+            }
+          case None => false
+        }
+      } else false
+
+
+  }
 
   def invoke(method: String, params: Json): Either[Throwable, Json] = {
     Try {
