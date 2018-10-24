f4e871cfdb21ae1098af7d3d1032068210215915
BelieveSong <swc0623@foxmail.com>
2018-10-24 18:34:53 +0800

    start implement node store handler

--- scp/src/main/scala/fssi/scp/interpreter/store/BallotStatus.scala
+++ scp/src/main/scala/fssi/scp/interpreter/store/BallotStatus.scala
@@ -1,24 +1,30 @@
 package fssi.scp.interpreter.store
 
+import fssi.scp.types.Message.BallotMessage
 import fssi.scp.types._
 
 case class BallotStatus(
-  phase: Var[Ballot.Phase]
+    phase: Var[Ballot.Phase],
+    latestEnvelopes: Var[Map[NodeID, Envelope[BallotMessage]]]
 )
 
 object BallotStatus {
   def empty: BallotStatus = BallotStatus(
-    phase = Var.empty
+    phase = Var.empty,
+    latestEnvelopes = Var.empty
   )
   private val instances: Var[Map[(NodeID, SlotIndex), BallotStatus]] = Var(Map.empty)
 
   def getInstance(nodeId: NodeID, slotIndex: SlotIndex): BallotStatus = {
-    instances.map(_.get((nodeId, slotIndex))).map {
-      case Some(b) => b
-      case None =>
-        val b = empty
-        instances := instances.unsafe() + ((nodeId, slotIndex) -> b)
-        b
-    }.unsafe
+    instances
+      .map(_.get((nodeId, slotIndex)))
+      .map {
+        case Some(b) => b
+        case None =>
+          val b = empty
+          instances := instances.unsafe() + ((nodeId, slotIndex) -> b)
+          b
+      }
+      .unsafe()
   }
 }
--- scp/src/main/scala/fssi/scp/interpreter/store/NominationStatus.scala
+++ scp/src/main/scala/fssi/scp/interpreter/store/NominationStatus.scala
@@ -1,5 +1,52 @@
 package fssi.scp.interpreter.store
+import fssi.scp.types._
+
+import scala.collection.immutable.TreeSet
 
 case class NominationStatus(
-  roundNumber: Var[Int]
+    roundNumber: Var[Int],
+    votes: Var[TreeSet[Value]], // X
+    accepted: Var[TreeSet[Value]], // Y
+    candidates: Var[TreeSet[Value]],                                   // Z
+    latestNominations: Var[Map[NodeID, Envelope[Message.Nomination]]], // N
+    // last envelope emitted by this node
+    lastEnvelope: Var[Envelope[Message.Nomination]],
+    // nodes from quorum set that have the highest priority this round
+    rounderLeaders: Var[TreeSet[NodeID]],
+    // true when nominate started
+    nominationStarted: Var[Boolean],
+    // the latest candidate value
+    latestCompositeCandidate: Var[Value],
+    // the value from the previous slot
+    previousValue: Var[Value]
 )
+
+object NominationStatus {
+
+  def empty =
+    NominationStatus(Var.empty,
+                     Var.empty,
+                     Var.empty,
+                     Var.empty,
+                     Var.empty,
+                     Var.empty,
+                     Var.empty,
+                     Var.empty,
+                     Var.empty,
+                     Var.empty)
+
+  private lazy val instances: Var[Map[(NodeID, SlotIndex), NominationStatus]] = Var(Map.empty)
+
+  def getInstance(nodeID: NodeID, slotIndex: SlotIndex): NominationStatus = {
+    instances
+      .map(_.get((nodeID, slotIndex)))
+      .map {
+        case Some(n) => n
+        case None =>
+          val n = empty
+          instances := instances.map(_ + ((nodeID, slotIndex) -> n)).unsafe()
+          n
+      }
+      .unsafe()
+  }
+}
