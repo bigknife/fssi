42268b15ce6f2fd43330fe83004e6ffcbc7889c1
BelieveSong <swc0623@foxmail.com>
2018-10-30 17:31:32 +0800

    add tool main program invoke

--- interpreter/src/main/scala/fssi/interpreter/ConsensusHandler.scala
+++ interpreter/src/main/scala/fssi/interpreter/ConsensusHandler.scala
@@ -1,15 +1,18 @@
 package fssi
 package interpreter
 import fssi.ast.Consensus
-import fssi.types.biz.{Node, Receipt, Transaction}
+import fssi.types.biz.Node.ConsensusNode
+import fssi.types.biz.{ConsensusAuxMessage, Receipt, Transaction}
 
 class ConsensusHandler extends Consensus.Handler[Stack] {
 
-  override def initialize(node: Node): Stack[Unit] = ???
+  override def initialize(node: ConsensusNode): Stack[Unit] = ???
 
   override def destroy(): Stack[Unit] = ???
 
   override def tryAgree(transaction: Transaction, receipt: Receipt): Stack[Unit] = ???
+
+  override def processMessage(message: ConsensusAuxMessage): Stack[Unit] = ???
 }
 
 object ConsensusHandler {
--- interpreter/src/main/scala/fssi/interpreter/NetworkHandler.scala
+++ interpreter/src/main/scala/fssi/interpreter/NetworkHandler.scala
@@ -1,77 +1,82 @@
 package fssi
 package interpreter
 import fssi.ast.Network
-import fssi.types.biz.{ChainConfiguration, JsonMessage, JsonMessageHandler, Node}
+import fssi.types.biz.Node.{ApplicationNode, ConsensusNode, ServiceNode}
+import fssi.types.biz._
+import fssi.types.{ApplicationMessage, ClientMessage, ConsensusMessage}
 import fssi.utils.Once
 import io.scalecube.cluster.{Cluster, ClusterConfig}
-import io.scalecube.transport.{Address, Message}
+import io.scalecube.transport.{Address, Message => CubeMessage}
 
 class NetworkHandler extends Network.Handler[Stack] with LogSupport {
 
   val clusterOnce: Once[Cluster] = Once.empty
 
-  override def startupPeerNode(conf: ChainConfiguration, handler: JsonMessageHandler): Stack[Node] =
-    Stack {
-      val config =
-        ClusterConfig
-          .builder()
-          .listenAddress(conf.host)
-          .port(conf.port)
-          .portAutoIncrement(false)
-          .seedMembers(conf.seeds.map(x => Address.create(x.host, x.port)): _*)
-          .suspicionMult(ClusterConfig.DEFAULT_WAN_SUSPICION_MULT)
-          .build()
+  override def startupConsensusNode(
+      conf: ChainConfiguration,
+      handler: Message.Handler[ConsensusMessage]): Stack[ConsensusNode] = Stack {
+    val config =
+      ClusterConfig
+        .builder()
+        .listenAddress(conf.host)
+        .port(conf.port)
+        .portAutoIncrement(false)
+        .seedMembers(conf.seeds.map(x => Address.create(x.host, x.port)): _*)
+        .suspicionMult(ClusterConfig.DEFAULT_WAN_SUSPICION_MULT)
+        .build()
 
-      clusterOnce := Cluster.joinAwait(config)
-      clusterOnce.foreach { cluster =>
-        cluster.listenMembership().subscribe { membershipEvent =>
-          log.info(
-            s"receive P2P Membership Event: ${membershipEvent.`type`()}, ${membershipEvent.member()}")
-          printMembers()
-        }
-
-        val subscription: Message => Unit = { gossip =>
-          scala.util.Try {
-            val jsonMessage = gossip.data[JsonMessage]()
-            log.debug("start to handle json message by gossiping")
-            handler.handle(jsonMessage)
-            log.debug("handled json message by gossiping")
-          } match {
-            case scala.util.Success(_) =>
-              log.debug("Gossip message handled successful")
-            case scala.util.Failure(e) =>
-              log.error("Gossip message handled failed", e)
-          }
-        }
-
-        cluster
-          .listenGossips()
-          .filter({ message =>
-            scala.util
-              .Try {
-                val jsonMessage = message.data[JsonMessage]
-                !handler.ignored(jsonMessage)
-              }
-              .toOption
-              .getOrElse(false): Boolean
-          })
-          .subscribe { gossip =>
-            log.debug(s"start handle Gossip message: $gossip")
-            subscription(gossip)
-          }
+    clusterOnce := Cluster.joinAwait(config)
+    clusterOnce.foreach { cluster =>
+      cluster.listenMembership().subscribe { membershipEvent =>
+        log.info(
+          s"receive P2P Membership Event: ${membershipEvent.`type`()}, ${membershipEvent.member()}")
+        printMembers()
       }
 
-      val node = Node(Node.Addr(conf.host, conf.port), conf.account)
-      log.info(s"node ($node) started ")
-      node
+      val subscription: CubeMessage => Unit = { gossip =>
+        scala.util.Try {
+          val consensusMessage = gossip.data[ConsensusMessage]()
+          log.debug("start to handle json message by gossiping")
+          handler(consensusMessage)
+          log.debug("handled json message by gossiping")
+        } match {
+          case scala.util.Success(_) =>
+            log.debug("Gossip message handled successful")
+          case scala.util.Failure(e) =>
+            log.error("Gossip message handled failed", e)
+        }
+      }
+
+      cluster
+        .listenGossips()
+        .subscribe { gossip =>
+          log.debug(s"start handle Gossip message: $gossip")
+          subscription(gossip)
+        }
+      ()
     }
 
-  override def shutdown(): Stack[Unit] = Stack {
+    val node = Node(Node.Addr(conf.host, conf.port), conf.account)
+    log.info(s"node ($node) started ")
+    ConsensusNode(node)
+  }
+
+  override def startupApplicationNode(
+      conf: ChainConfiguration,
+      handler: Message.Handler[ApplicationMessage]): Stack[ApplicationNode] = ???
+
+  override def startupServiceNode(conf: ChainConfiguration,
+                                  handler: Message.Handler[ClientMessage]): Stack[ServiceNode] = ???
+
+  override def shutdownConsensusNode(node: ConsensusNode): Stack[Unit] = Stack {
     clusterOnce.foreach { cluster =>
       cluster.shutdown().get()
       log.info(s"node ${cluster.address()} shutdown")
     }
   }
+  override def shutdownApplicationNode(node: ApplicationNode): Stack[Unit] = ???
+
+  override def shutdownServiceNode(node: ServiceNode): Stack[Unit] = ???
 
   private def printMembers(): Unit = {
     log.info("current members")
