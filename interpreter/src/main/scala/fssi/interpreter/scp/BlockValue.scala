50a42309dd28268e1e72734279f8496b51f248e1
bigknife <bigknife@outlook.com>
2018-11-12 13:40:19 +0800

    add BlockValue

--- interpreter/src/main/scala/fssi/interpreter/scp/BlockValue.scala
+++ interpreter/src/main/scala/fssi/interpreter/scp/BlockValue.scala
@@ -1,15 +1,10 @@
 package fssi.interpreter.scp
 
-import java.nio.ByteBuffer
-
-import fssi.base.BytesValue
 import fssi.scp.types._
 import fssi.store.mpt.Hash
-import fssi.store.implicits._
 import fssi.types.TransactionSet
-import BytesValue.implicits._
-import fssi.types.biz.Transaction._
-import fssi.base.BytesValue.implicits._
+import fssi.types.implicits._
+import fssi.store.implicits._
 
 case class BlockValue(
     height: BigInt,
@@ -22,7 +17,8 @@ case class BlockValue(
 ) extends Value {
 
   override def rawBytes: Array[Byte] = {
-    // height + previousBlockHash + currentBlockHash + previousStateHash + currentStateHash + timestamp + transactions
+    // height + previousBlockHash + currentBlockHash +
+    // previousStateHash + currentStateHash + timestamp + transactions
     val transactionBytes = transactions.foldLeft(Array.emptyByteArray) { (acc, n) =>
       acc ++ n.asBytesValue.bytes
     }
@@ -31,20 +27,23 @@ case class BlockValue(
       currentBlockHash.asBytesValue.any ++ previousStateHash.asBytesValue.any ++
       currentStateHash.asBytesValue.any ++ timestamp.value.asBytesValue.any).bytes ++
       transactionBytes
+
   }
 
-  override def compare(v: Value): Int = v match {
-    case that: BlockValue =>
-      val heightOrder = Ordering[BigInt].compare(this.height, that.height)
-      if (heightOrder != 0) heightOrder
-      else {
-        val tsOrder = Ordering[Long].compare(this.timestamp.value, that.timestamp.value)
-        if (tsOrder != 0) tsOrder
+  override def compare(v: Value): Int =
+    v match {
+      case that: BlockValue =>
+        val heightOrder = Ordering[BigInt].compare(this.height, that.height)
+        if (heightOrder != 0) heightOrder
         else {
-          val thisEncoding = rawBytes.asBytesValue.bcBase58
-          val thatEncoding = that.rawBytes.asBytesValue.bcBase58
-          Ordering[String].compare(thisEncoding, thatEncoding)
+          val tsOrder = Ordering[Long].compare(this.timestamp.value, that.timestamp.value)
+          if (tsOrder != 0) tsOrder
+          else {
+            val thisEncoding = rawBytes.asBytesValue.bcBase58
+            val thatEncoding = that.rawBytes.asBytesValue.bcBase58
+            Ordering[String].compare(thisEncoding, thatEncoding)
+          }
         }
-      }
-  }
+    }
+
 }
