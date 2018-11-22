ccc4b6a627cca78e4305cb71964d4f1e5aeec71d
BelieveSong <swc0623@foxmail.com>
2018-11-22 15:35:50 +0800

    remove password parameter

--- core-node/src/main/scala/fssi/corenode/CoreNodeSettingParser.scala
+++ core-node/src/main/scala/fssi/corenode/CoreNodeSettingParser.scala
@@ -13,10 +13,4 @@ object CoreNodeSettingParser extends OptionParser[Setting.CoreNodeSetting]("core
     .abbr("w")
     .text("working directory")
     .action((x, c) => c.copy(workingDir = x))
-
-  opt[String]("password")
-    .abbr("p")
-    .required()
-    .text("password of the bound account")
-    .action((x, c) => c.copy(password = x.getBytes("utf-8")))
 }
