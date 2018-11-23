1de92ad8bad293496521578d7f598f3c9fdaa871
BelieveSong <swc0623@foxmail.com>
2018-11-23 18:03:09 +0800

    add debug info for Statement

--- scp/src/main/scala/fssi/scp/types/Statement.scala
+++ scp/src/main/scala/fssi/scp/types/Statement.scala
@@ -1,4 +1,5 @@
 package fssi.scp.types
+import org.slf4j.LoggerFactory
 
 case class Statement[M <: Message](
     from: NodeID,
@@ -14,11 +15,29 @@ case class Statement[M <: Message](
 }
 
 object Statement {
+  private val log = LoggerFactory.getLogger(getClass)
   trait Implicits {
     import fssi.base.implicits._
     import fssi.scp.types.implicits._
+    import fssi.utils.crypto
 
-    implicit def statementToBytes[M <: Message](statement: Statement[M]): Array[Byte] =
-      statement.from.asBytesValue.bytes ++ statement.slotIndex.asBytesValue.bytes ++ statement.timestamp.asBytesValue.bytes ++ statement.quorumSet.asBytesValue.bytes ++ statement.message.asBytesValue.bytes
+    implicit def statementToBytes[M <: Message](statement: Statement[M]): Array[Byte] = {
+      val from      = statement.from.asBytesValue.any
+      val slotIndex = statement.slotIndex.asBytesValue.any
+      val timestamp = statement.timestamp.asBytesValue.any
+      val quorum    = statement.quorumSet.asBytesValue.any
+      val message   = statement.message.asBytesValue.any
+
+      log.error("=============================================")
+      log.error(s"from ${from.bcBase58}")
+      log.error(s"      hash from: ${crypto.sha3(from.bytes).asBytesValue.bcBase58}")
+      log.error(s"      hash slotIndex: ${crypto.sha3(slotIndex.bytes).asBytesValue.bcBase58}")
+      log.error(s"      hash timestamp: ${crypto.sha3(timestamp.bytes).asBytesValue.bcBase58}")
+      log.error(s"      hash quorum: ${crypto.sha3(quorum.bytes).asBytesValue.bcBase58}")
+      log.error(s"      hash messdage: ${crypto.sha3(message.bytes).asBytesValue.bcBase58}")
+      log.error("=============================================")
+
+      (from ++ slotIndex ++ timestamp ++ quorum ++ message).bytes
+    }
   }
 }
