e70513fa8e0c7009e931ad65d9107edbca59b22b
zhanhai <zhanhai@weibopay.com>
2018-11-26 13:51:52 +0800

    Add SCP unit spec suites

--- scp/src/test/scala/fssi/scp/SCPSpec.scala
+++ scp/src/test/scala/fssi/scp/SCPSpec.scala
@@ -1,34 +1,9 @@
 package fssi.scp
 
-import fssi.scp.ast.components._
-import fssi.scp.ast.uc._
-import fssi.scp.interpreter._
-import fssi.scp.types._
 import org.scalatest._
 
-import scala.collection.immutable._
-
-class SCPSpec extends FunSuite with TestSupport {
-  val scp = SCP[Model.Op]
-
-  test("ast") {
-    val (nodeId, nodeKey)    = createNodeID()
-    val slotIndex            = SlotIndex(1)
-    val value: Value         = TestValue(TreeSet(1, 2))
-    val previousValue: Value = TestValue(TreeSet(1))
-
-    val setting = Setting(
-      quorumSet = QuorumSet.slices(
-        QuorumSet.Slices.flat(1, nodeId)
-      ),
-      localNode = nodeId,
-      privateKey = null,
-      applicationCallback = null,
-      broadcastTimeout = 0
-    )
-
-    val p = scp.handleAppRequest(nodeId, slotIndex, value, previousValue)
-    val r = runner.runIO(p, setting).unsafeRunSync
-    info(s"r = $r")
-  }
-}
+class SCPSpec
+    extends Suites(
+      new BallotSpec,
+      new NominationSpec
+    ) {}
