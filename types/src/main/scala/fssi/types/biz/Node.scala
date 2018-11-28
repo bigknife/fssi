28118d6a8bc2dc7bc1a0afa8b05892ac3d42d625
Benny Song <bigknife@outlook.com>
2018-11-28 15:40:35 +0800

    fix app & consensus worker

--- types/src/main/scala/fssi/types/biz/Node.scala
+++ types/src/main/scala/fssi/types/biz/Node.scala
@@ -1,20 +1,28 @@
 package fssi.types.biz
 
+import fssi.types.implicits._
+
 case class Node(
-  address: Node.Addr,
-  account: Account
-)
+    address: Node.Addr,
+    account: Account
+) {
+
+  private lazy val show: String =
+    s"$address(${account.id.asBytesValue.bcBase58}:${account.pubKey.asBytesValue.bcBase58})"
+
+  override def toString: String = show
+}
 
 object Node {
-  case class ConsensusNode(value: Node) extends AnyVal
+  case class ConsensusNode(value: Node)   extends AnyVal
   case class ApplicationNode(value: Node) extends AnyVal
-  case class ServiceNode(value: Node) extends AnyVal
+  case class ServiceNode(value: Node)     extends AnyVal
 
   case class Addr(host: String, port: Int) {
     override def toString(): String = s"$host:$port"
   }
   def parseAddr(addr: String): Option[Addr] = addr.split(":") match {
-    case Array(ip, port) => scala.util.Try {Addr(ip, port.toInt)}.toOption
-    case _ => None
+    case Array(ip, port) => scala.util.Try { Addr(ip, port.toInt) }.toOption
+    case _               => None
   }
 }
