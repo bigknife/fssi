e2c77392ac915dc4755854ee75ae2c6c80c9b7e9
Benny Song <bigknife@outlook.com>
2018-12-07 16:40:03 +0800

    tuning: broadcast last tow solt messages regularly

--- scp/src/main/scala/fssi/scp/ast/uc/BroadcastMessageProgram.scala
+++ scp/src/main/scala/fssi/scp/ast/uc/BroadcastMessageProgram.scala
@@ -20,19 +20,36 @@ trait BroadcastMessageProgram[F[_]] extends SCP[F] with BaseProgram[F] {
       nextSlotIndex = slotIndex + 1
       timeout         <- broadcastTimeout()
       _               <- debug(s"broadcast $nextSlotIndex message regularly,timeout: $timeout millis")
-      nominateMessage <- nominateEnvelope(nextSlotIndex)
-      _ <- ifThen(nominateMessage.nonEmpty) {
+
+      nominateMessage0 <- nominateEnvelope(slotIndex)
+      _ <- ifThen(nominateMessage0.nonEmpty) {
         for {
-          _ <- broadcastEnvelope(nextSlotIndex, nominateMessage.get)
-          _ <- infoSentEnvelope(nominateMessage.get)
+          _ <- broadcastEnvelope(slotIndex, nominateMessage0.get)
+          _ <- infoSentEnvelope(nominateMessage0.get)
         } yield ()
       }
 
-      ballotMessage <- ballotEnvelope(nextSlotIndex)
-      _ <- ifThen(ballotMessage.nonEmpty) {
+      nominateMessage1 <- nominateEnvelope(nextSlotIndex)
+      _ <- ifThen(nominateMessage1.nonEmpty) {
         for {
-          _ <- broadcastEnvelope(nextSlotIndex, ballotMessage.get)
-          _ <- infoSentEnvelope(ballotMessage.get)
+          _ <- broadcastEnvelope(nextSlotIndex, nominateMessage1.get)
+          _ <- infoSentEnvelope(nominateMessage1.get)
+        } yield ()
+      }
+
+      ballotMessage0 <- ballotEnvelope(slotIndex)
+      _ <- ifThen(ballotMessage0.nonEmpty) {
+        for {
+          _ <- broadcastEnvelope(slotIndex, ballotMessage0.get)
+          _ <- infoSentEnvelope(ballotMessage0.get)
+        } yield ()
+      }
+
+      ballotMessage1 <- ballotEnvelope(nextSlotIndex)
+      _ <- ifThen(ballotMessage1.nonEmpty) {
+        for {
+          _ <- broadcastEnvelope(nextSlotIndex, ballotMessage1.get)
+          _ <- infoSentEnvelope(ballotMessage1.get)
         } yield ()
       }
       _ <- delayExecuteProgram(BROADCAST_TIMER, broadcastMessageRegularly(), timeout)
