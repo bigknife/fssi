4dff4c8008cfa72593c8e2f9c9cfce73c53b10e3
BelieveSong <swc0623@foxmail.com>
2019-06-27 16:53:02 +0800

    add document

--- contract-scaffold/src/main/resources/META-INF/InterfaceSample.java
+++ contract-scaffold/src/main/resources/META-INF/InterfaceSample.java
@@ -2,18 +2,19 @@ package com.fssi.sample;
 
 import fssi.contract.lib.Context;
 import fssi.contract.lib.KVStore;
-import fssi.contract.lib.SqlStore;
 import fssi.contract.lib.TokenQuery;
 
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Map;
 
 public class InterfaceSample {
 
     public void tokenQuerySample(Context context, String accountId) {
         TokenQuery tokenQuery = context.tokenQuery();
         Long amount = tokenQuery.getAmount(accountId);
+        System.out.println("============= SUCCESS: got accountId " + accountId + " token :" + amount + " ==========");
+
+        String currentAccount = context.currentAccountId();
+        System.out.println("============= SUCCESS: got current accountId " + currentAccount + " =====");
     }
 
     public void kvStoreSample(Context context, String key, String value) {
@@ -24,30 +25,16 @@ public class InterfaceSample {
 
         // store to kv store
         kvStore.put(keyBytes, valueBytes);
+        System.out.println("========= SUCCESS: put key [" + key + "] to store on value [" + value + "] ======");
 
         // get from kv store
         byte[] vBytes = kvStore.get(keyBytes);
+        String storeValue = new String(vBytes, StandardCharsets.UTF_8);
+        System.out.println("========= SUCCESS: get key [" + key + "] in store ,found value [" + storeValue + "] =====");
     }
 
-    public void sqlStoreSample(Context context, int arg1, double arg2) {
-        try {
-            SqlStore sqlStore = context.sqlStore();
-            String createTable = "create table if not exists t_arg (arg1 int, arg2 double)";
-            // create table
-            sqlStore.executeCommand(createTable);
-
-            String queryData = "select arg1,arg2 from t_arg where arg1 = ? and arg2 = ?";
-            List<Map<String, Object>> mapList = sqlStore.executeQuery(queryData, arg1, arg2);
-
-            // do something
-
-            // if you want to open transaction try followed method
-            sqlStore.startTransaction();
-            sqlStore.commit();
-            sqlStore.rollback();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void currentAccountIdSample(Context context) {
+        String currentAccountId = context.currentAccountId();
+        System.out.println("========= SUCCESS: get current account id [" + currentAccountId + "] =====");
     }
-
 }
